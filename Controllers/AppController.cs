386c0f76bc13e95af7523d4e4fb7a0d058da3448
szntech <zevweiss639@gmail.com>
2022-12-14 12:48:49 -0500

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,119 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Net;
+using Intuit.Ipp.Core;
+using Intuit.Ipp.Data;
+using Intuit.Ipp.QueryFilter;
+using Intuit.Ipp.Security;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Ajax.Utilities;
+
+namespace MvcCodeFlowClientManual.Controllers
+{
+    public class AppController : Controller
+    {
+        public static string clientid = ConfigurationManager.AppSettings["clientid"];
+        public static string clientsecret = ConfigurationManager.AppSettings["clientsecret"];
+        public static string redirectUrl = ConfigurationManager.AppSettings["redirectUrl"];
+        public static string environment = ConfigurationManager.AppSettings["appEnvironment"];
+
+        public static OAuth2Client auth2Client = new OAuth2Client(clientid, clientsecret, redirectUrl, environment);
+
+        /// <summary>
+        /// Use the Index page of App controller to get all endpoints from discovery url
+        /// </summary>
+        public ActionResult Index()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Session.Clear();
+            Session.Abandon();
+            Request.GetOwinContext().Authentication.SignOut("Cookies");
+            return View();
+        }
+
+        /// <summary>
+        /// Start Auth flow
+        /// </summary>
+        public ActionResult InitiateAuth(string submitButton)
+        {
+            switch (submitButton)
+            {
+                case "Connect to QuickBooks":
+                    List<OidcScopes> scopes = new List<OidcScopes>();
+                    scopes.Add(OidcScopes.Accounting);
+                    string authorizeUrl = auth2Client.GetAuthorizationURL(scopes);
+                    return Redirect(authorizeUrl);
+                default:
+                    return (View());
+            }
+        }
+
+        /// <summary>
+        /// QBO API Request
+        /// </summary>
+        public ActionResult ApiCallService(string submitButton)
+        {
+            if(submitButton == "refresh")
+            {
+                Session["output"] = null;
+            }
+            if (Session["output"] != null)
+            {
+                return View("ApiCallService", Session["output"]);
+            }
+               else if (Session["realmId"] != null)
+            {
+                string realmId = Session["realmId"].ToString();
+                try
+                {
+                    var principal = User as ClaimsPrincipal;
+                    OAuth2RequestValidator oauthValidator = new OAuth2RequestValidator(principal.FindFirst("access_token").Value);
+
+                    // Create a ServiceContext with Auth tokens and realmId
+                    ServiceContext serviceContext = new ServiceContext(realmId, IntuitServicesType.QBO, oauthValidator);
+                    serviceContext.IppConfiguration.MinorVersion.Qbo = "23";
+
+                    QueryService<Customer> querySvc = new QueryService<Customer>(serviceContext);
+                    Customer[] companyInfo = querySvc.ExecuteIdsQuery("SELECT CompanyName FROM Customer ").ToArray<Customer>();
+                    string output = "";
+                    companyInfo.ForEach<Customer>(company =>
+                    {
+                        output += "Company Name: " + company.CompanyName +", ";
+                    });
+                    Session["output"] = output;
+                    return View("ApiCallService", (object)(output));
+                
+                }
+                catch (Exception ex)
+                {
+                    return View("ApiCallService", (object)("QBO API call Failed!" + " Error message: " + ex.Message));
+                }
+            }
+            else
+                return View("ApiCallService", (object)"QBO API call Failed!");
+        }
+
+        /// <summary>
+        /// Use the Index page of App controller to get all endpoints from discovery url
+        /// </summary>
+        public ActionResult Error()
+        {
+            return View("Error");
+        }
+
+        /// <summary>
+        /// Action that takes redirection from Callback URL
+        /// </summary>
+        public ActionResult Tokens()
+        {
+            return View("Tokens");
+        }
+    }
+}
